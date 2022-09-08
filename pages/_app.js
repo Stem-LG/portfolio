f3252c1c73eb12cf0420c25dc478d7d169f6687e
louay ghanney <louayghanney71@gmail.com>
2022-09-08 19:45:54 +0100

    finish most of the desktop ui and finished form validation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Head from "next/head";
+import { ThemeProvider, FormControlLabel, Switch } from "@mui/material";
+import { lightTheme, darkTheme } from "../lib/theme";
+import { useEffect, useState, pro } from "react";
+import { appWithTranslation } from "next-i18next";
+import DarkContext from "../lib/contexts/darkmode";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [isDarkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        setDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>Louay's Portfolio</title>
+                <meta
+                    name="description"
+                    content="Personal Porfolio for Louay Ghanney"
+                />
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                ></meta>
+            </Head>
+            <DarkContext.Provider value={{ isDarkMode, setDarkMode }}>
+                <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </DarkContext.Provider>
+        </>
+    );
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
