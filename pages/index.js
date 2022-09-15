ec8e8b0788a85cd32d66d460aa320c1333d3c423
louay ghanney <louayghanney71@gmail.com>
2022-09-15 03:04:16 +0100

    mobile view improvements

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,33 @@
-import { useTheme } from "@mui/material";
+import { useTheme, Button } from "@mui/material";
 import TopAppBar from "../lib/components/appbar";
 import DarkModeFab from "../lib/components/fab";
 import Home from "../lib/sections/home";
 import Projects from "../lib/sections/projects";
 import Contact from "../lib/sections/contact";
+// import { useState } from "react";
+// import { useRef } from "react";
+
+// import useVisiblity from "../lib/hooks/visibility";
+// import { useEffect } from "react";
 
 export default function Index(props) {
-    const theme = useTheme();
+    // const projectsRef = useRef();
+
+    // const projectsIsVisible = useVisiblity(projectsRef);
+
+
+    // // setInterval(() => {
+    // //     console.log(projectsIsVisible);
+    // // }, 1000);
+
+    // console.log(projectsRef);
+
     return (
         <>
             <TopAppBar />
-            <Home />
-            <Projects />
-            <Contact />
+            <Home id="home" />
+            <Projects id="projects" />
+            <Contact id="contact" />
             <DarkModeFab />
         </>
     );
