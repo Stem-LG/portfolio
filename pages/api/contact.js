0966fd27f0c97a56c01836593464facb1209d979
louay ghanney <louayghanney71@gmail.com>
2022-09-19 22:34:06 +0100

    implement the contact backend

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,38 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import client from "../../lib/mongodb";
+import { emailRegex } from "../../lib/regexPatterns";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+//name email subject message
+
+export default async (req, res) => {
+    let db = client.db(process.env.MONGODB_DBNAME);
+    let collection = db.collection("Messages");
+
+    let message = {
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.content,
+    };
+
+    if (verifyData(message)) {
+        let exist = await collection.findOne(message);
+
+        if (!exist) {
+            collection.insertOne(message);
+            res.json({ exist: false, invalid: false });
+        } else {
+            res.json({ exist: true, invalid: false });
+        }
+    } else {
+        res.json({ invalid: true });
+    }
+};
+
+function verifyData(data) {
+    let nameValid = data.name.length > 2 && data.name.length <= 40;
+    let emailValid = emailRegex.test(data.email) && data.email.length < 200;
+    let subjectValid = data.subject.length <= 100;
+    let messageValid = data.message.length >= 15 && data.message.length <= 500;
+
+    return nameValid && emailValid && subjectValid && messageValid;
 }
